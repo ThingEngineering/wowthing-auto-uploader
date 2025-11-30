636fc20211720ff5c380c5a0766e0d8c8179c4db
Freddie <freddie@wafflemonster.org>
2025-11-30 14:54:28 -0800

    Very basic Avalonia setup with some UI

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace WowthingAutoUploader;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void HandleWowFolderTapped(object sender, RoutedEventArgs e)
+    {
+        // meow
+        
+    }
+}
